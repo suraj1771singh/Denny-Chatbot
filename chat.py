673bd49687672ff501b19a5e57f0c0b466302508
Suraj Singh <absurajsinghchamiyal@gmail.com>
2024-06-26 15:13:01 +0530

    Integeration Chat with streamlit

--- chat.py
+++ chat.py
@@ -3,40 +3,57 @@ from langchain_core.output_parsers import StrOutputParser
 from models import get_gemini_model, get_llama_model
 from prompts import get_chat_prompt
 from utils import bind_memory
+from typing import Dict, Any
 
 
-def main():
-
-    try:
-
-        chat_model = get_llama_model()
-        prompt = get_chat_prompt()
-        parser = StrOutputParser()
+class Chat:
+    def __init__(self) -> None:
+        self.chat_model = get_llama_model()
+        self.prompt = get_chat_prompt()
+        self.parser = StrOutputParser()
 
         # Create the chain from the prompt and the model and parser
-        chain = prompt | chat_model | parser
+        self.chain = self.prompt | self.chat_model | self.parser
 
         # Add the memory to the chain
-        store = {}
-        config = {"configurable": {"session_id": "abc2"}}
-        chain = bind_memory(store, chain)
+        self.store: Dict[str, Any] = {}
+        self.config: Dict[str, Any] = {"configurable": {"session_id": "abc2"}}
+        self.chain = bind_memory(self.store, self.chain)
 
-        while True:
-            try:
-                user_input = input("You: ")
-                if user_input.lower() in ["exit", "quit", "bye"]:
-                    print("\nLeaving...")
-                    break
-
-                # Invoke the chain with the user's message
-                res = chain.invoke(HumanMessage(user_input), config=config)
-                print(f"Sarah: {res}")
-            except KeyboardInterrupt:
-                print("\nLeaving...")
-                break
-    except Exception as e:
-        print(e)
+    def generate_response(self, user_input: str):
+        try:
+            # Invoke the chain with the user input and configuration
+            response = self.chain.stream(HumanMessage(user_input), config=self.config)
+            return response
+        except Exception as e:
+            # Handle any exceptions gracefully
+            return f"Error occurred: {str(e)}"
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+
+#     try:
+
+#         chat_model = get_llama_model()
+#         prompt = get_chat_prompt()
+#         parser = StrOutputParser()
+
+# while True:
+#     try:
+#         user_input = input("You: ")
+#         if user_input.lower() in ["exit", "quit", "bye"]:
+#             print("\nLeaving...")
+#             break
+
+#         # Invoke the chain with the user's message
+#         res = chain.invoke(HumanMessage(user_input), config=config)
+#         print(f"Sarah: {res}")
+#     except KeyboardInterrupt:
+#         print("\nLeaving...")
+#         break
+#     except Exception as e:
+#         print(e)
+
+
+# if __name__ == "__main__":
+#     main()
--- main.py
+++ main.py
@@ -1,60 +1,37 @@
-# Python imports
+import streamlit as st
+from chat import Chat
 from dotenv import load_dotenv
-import logging
 
-# Load API Keys
 load_dotenv()
 
-# Langchain imports
-
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.output_parsers import StrOutputParser
-
-from langgraph.prebuilt import create_react_agent
-from langgraph.checkpoint.sqlite import SqliteSaver
-from models import get_gemini_model
-from tools import get_tools
+st.title("🦜 Sarah Chat Assistant")
 
 
-# Set up logging
-logging.getLogger().setLevel(logging.ERROR)
-
-model = get_gemini_model()
-tools = get_tools()
-
-store = {}
+@st.cache_resource
+def load_model():
+    return Chat()
 
 
+# Load LLM Model
+chat_model = load_model()
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 
-agent_executor = create_react_agent(model, tools)
-memory = SqliteSaver.from_conn_string(":memory:")
+prompt = st.chat_input("Say something")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-
-
-
-
-config = {"configurable": {"thread_id": "abc123"}}
-
-
-
-chain = prompt | agent_executor
-parser = StrOutputParser()
-
-while True:
-    try:
-        user_input = input("You : ")
-        print("AI :", end="")
-        for r in agent_executor.stream(
-            {"messages": [HumanMessage(content=user_input)]}, config
-        ):
-
-            print(r["agent"]["messages"][0].content, end="")
-        print("")
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        # save_store()
-        exit(0)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(chat_model.generate_response(prompt))
+    st.session_state.messages.append({"role": "assistant", "content": response})
